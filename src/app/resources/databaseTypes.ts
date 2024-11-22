4a031834ec59895efad3aec615f3006e01bc5f91
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2024-11-22 22:47:35 +1300

    Added a type to define what to expect from the database on query result

---
+++ src/app/resources/databaseTypes.ts
@@ -0,0 +1,13 @@
+type userDBSchema = {
+	id: string;
+	first_name: string;
+	last_name: string;
+	username: string;
+	phone_number: string;
+	email: string;
+	password: string;
+	email_validated: number;
+	phone_number_validated: number;
+};
+
+export { userDBSchema };
