c227a5f124faf6212f9a5951073e251f048ff019
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2024-12-17 19:43:34 +1300

    Improve type definition

--- src/app/middlewares/cors.ts
+++ src/app/middlewares/cors.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export default (req: Request, res: Response, next: () => void) => {
+export default (req: Request, res: Response, next: NextFunction) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
