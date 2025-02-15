44b4a5da8b69251b8710ab4763e2fa505879f7ed
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-02-15 15:17:20 +1300

    Added teh vehicle repository with the following functions getVehicleById, postVehicle, deleteVehicleWithId, updateVehicleWithId

---
+++ src/app/Repositories/vehicleRepository.ts
@@ -0,0 +1,104 @@
+import { getPool } from '../../config/db';
+import logger from '../../config/logger';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { VehicleListingDBSchema, Vehicle } from '../resources/types';
+
+const vehicleDatabaseFields: { [key: string]: string } = {
+	price: 'price',
+	photoPaths: 'photo_paths',
+	uploadDate: 'upload_date',
+	description: 'description',
+	endDate: 'end_date',
+	endTime: 'end_time',
+	make: 'make',
+	model: 'model',
+	year: 'year',
+	kilometers: 'kilometers',
+	fuelType: 'fuel_type',
+	bodyType: 'body_type',
+	driveType: 'drive_type',
+	orcIncluded: 'orc_included',
+	numberPlate: 'number_plate',
+	seats: 'seats',
+	doors: 'doors',
+	previousOwners: 'previous_owners',
+	color: 'color',
+	engineSize: 'engine_size',
+	transmission: 'transmission',
+	cylinders: 'cylinders',
+	regoExpiryDate: 'rego_expiry_date',
+	wofExpiryDate: 'wof_expiry_date',
+};
+
+function getAllVehicles() {
+	logger.info('Getting all vehicles from the database');
+}
+
+async function getVehicleById(id: string) {
+	logger.info(`Getting vehicle with id '${id}' from the database`);
+
+	const conneciton = await getPool().getConnection();
+	const query = 'SELECT * FROM vehicle_listing WHERE id = ?';
+	const [result] = await conneciton.query<RowDataPacket[]>(query, [id]);
+	conneciton.release();
+
+	return result as VehicleListingDBSchema[];
+}
+
+async function postVehicle(vehicleData: Omit<Vehicle, 'id'>) {
+	logger.info('Adding new vehicle');
+
+	const entries = Object.entries(vehicleData);
+	const fields = entries
+		.map(([key]) => vehicleDatabaseFields[key])
+		.join(', ');
+	const values = entries.map(([, value]) => value);
+
+	const connection = await getPool().getConnection();
+	const query = `INSERT INTO vehicle_listing (${fields}) 
+					values (${values.map(() => '?').join(', ')})`;
+
+	const [result] = await connection.query<ResultSetHeader>(query, values);
+	connection.release();
+
+	return result;
+}
+
+async function deleteVehicleWithId(id: string) {
+	logger.info(`Deleting vehicle with id '${id}' from the database`);
+
+	const connection = await getPool().getConnection();
+	const query = 'DELETE FROM vehicle_listing WHERE id = ?';
+	const [result] = await connection.query<ResultSetHeader>(query, [id]);
+	connection.release();
+
+	return result;
+}
+
+async function updateVehicleWithId(
+	id: string,
+	updateValues: Omit<Partial<Vehicle>, 'id' | 'userIdFk'>
+) {
+	logger.info(`Updating vehicle with id '${id}' in the database`);
+
+	if (Object.keys(updateValues).length === 0) {
+		logger.error('Trying to update vehicle with no update values');
+		throw new Error('Empty vehicle update fields');
+	}
+
+	const fields = Object.keys(updateValues)
+		.map((key) => `${vehicleDatabaseFields[key]} = ?`)
+		.join(', ');
+
+	const values = Object.values(updateValues);
+
+	const connection = await getPool().getConnection();
+	const query = `UPDATE vehicle_listing SET ${fields} WHERE id = ?`;
+	const [result] = await connection.query<ResultSetHeader>(query, [
+		values,
+		id,
+	]);
+	connection.release();
+
+	return result;
+}
