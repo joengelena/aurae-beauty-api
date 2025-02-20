2948a55c3196079c3258db1a2bb0ebcd694080fe
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-02-20 22:11:39 +1300

    Update the vehicle route to call a vehicle repo method

--- src/app/routes/vehicle.routes.ts
+++ src/app/routes/vehicle.routes.ts
@@ -3,15 +3,20 @@ import { rootUrl } from './base.routes';
 import validateRequest from '../middlewares/requestAuthentication/validateRequest';
 import validateRequestBody from '../middlewares/validateRequestBody';
 import ajvSchema from '../resources/ajvSchema.json';
+import { postVehicle } from '../controllers/vehicleController';
 
 const vehicleRoutes = (app: Express) => {
 	app.route(rootUrl + '/vehicles')
 		.get((req, res, next) => {
 			validateRequestBody(req, res, next, ajvSchema.emptyBody);
 		})
-		.post(validateRequest, (req, res, next) => {
-			validateRequestBody(req, res, next, ajvSchema.emptyBody);
-		});
+		.post(
+			validateRequest,
+			(req, res, next) => {
+				validateRequestBody(req, res, next, ajvSchema.postVehicle);
+			},
+			postVehicle
+		);
 
 	app.route(rootUrl + '/vehicles/:id')
 		.get((req, res, next) => {
