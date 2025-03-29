7988aef721bcee1dcef1cea846299207c859e4fb
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-03-29 16:42:20 +1300

    Update the email service functions to not have the url and path as a param and instead the functions themselves to retreive the information from the appConfigRepo themselves

--- src/app/utils/email/emailService.ts
+++ src/app/utils/email/emailService.ts
@@ -2,36 +2,47 @@ import { generateJwtToken } from '../jwt/generateJwt';
 import nodemailerSendEmail from './nodemailer';
 import getValidateEmailFormat from './getValidateEmailFormat';
 import getForgotPasswordEmailFormat from './getForgotPasswordEmailFormat';
+import * as appConfigRepository from '../../repositories/appConfigurationRepository/appConfigurationRepository';
 
-async function sendEmailVerificationLink(
-	userId: string,
-	email: string,
-	webAppBaseUrl: string,
-	urlPath: string
-) {
+async function sendEmailVerificationLink(userId: string, email: string) {
+	const appConfig = await appConfigRepository.getAppConfig();
+
+	const webAppBaseUrl = appConfig.find(
+		(config) => config.name === 'webAppBaseUrl'
+	).value;
+
+	const verifyEmailUrlPath = appConfig.find(
+		(config) => config.name === 'verifyEmailUrlPath'
+	).value;
+
 	const emailVerificationJwt = generateJwtToken({ userId: userId }, '15m');
 
 	const emailSubject = `${process.env.COMPANY_NAME} - Email Verification`;
 
 	const htmlBody = getValidateEmailFormat(
-		`${webAppBaseUrl}${urlPath}?token=${emailVerificationJwt}`
+		`${webAppBaseUrl}${verifyEmailUrlPath}?token=${emailVerificationJwt}`
 	);
 
 	await nodemailerSendEmail(email, emailSubject, htmlBody);
 }
 
-async function sendResetPasswordLink(
-	userId: string,
-	email: string,
-	webAppBaseUrl: string,
-	urlPath: string
-) {
+async function sendResetPasswordLink(userId: string, email: string) {
+	const appConfig = await appConfigRepository.getAppConfig();
+
+	const webAppBaseUrl = appConfig.find(
+		(config) => config.name === 'webAppBaseUrl'
+	).value;
+
+	const resetPasswordUrlPath = appConfig.find(
+		(config) => config.name === 'resetPasswordUrlPath'
+	).value;
+
 	const emailSubject = `${process.env.COMPANY_NAME} - Reset Password`;
 
 	const resetPasswordJwt = generateJwtToken({ userId: userId }, '15m');
 
 	const htmlBody = getForgotPasswordEmailFormat(
-		`${webAppBaseUrl}${urlPath}?token=${resetPasswordJwt}`
+		`${webAppBaseUrl}${resetPasswordUrlPath}?token=${resetPasswordJwt}`
 	);
 
 	await nodemailerSendEmail(email, emailSubject, htmlBody);
