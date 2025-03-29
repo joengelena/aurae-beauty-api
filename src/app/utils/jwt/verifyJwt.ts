4a15ceb802e57d9ed0ec961cd975223ce728aaba
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-03-29 16:31:48 +1300

    Add an enum for the jwt status

--- src/app/utils/jwt/verifyJwt.ts
+++ src/app/utils/jwt/verifyJwt.ts
@@ -1,20 +1,27 @@
 import jwt from 'jsonwebtoken';
 
+enum JwtStatus {
+	VALID = 'valid',
+	INVALID = 'invalid',
+	EXPIRED = 'expired',
+}
+
 function verifyJwt(jwtToken: string) {
 	try {
 		const validJwt = jwt.verify(jwtToken, process.env.JWT_SECRET);
-		return { status: 'valid', data: validJwt };
+		return { status: JwtStatus.VALID, data: validJwt };
 	} catch (error) {
 		if (error.code === 'TokenExpiredError') {
-			return { status: 'expired', data: '' };
+			return { status: JwtStatus.EXPIRED, data: '' };
 		}
 
 		if (error.code === 'JsonWebTokenError') {
-			return { status: 'invalid', data: '' };
+			return { status: JwtStatus.INVALID, data: '' };
 		}
 
-		return { status: 'invalid', data: '' };
+		return { status: JwtStatus.INVALID, data: '' };
 	}
 }
 
 export default verifyJwt;
+export { JwtStatus };
