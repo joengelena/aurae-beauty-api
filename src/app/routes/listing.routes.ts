9b83fdad0377d4b24df6813f0a1eeb0b32489cfc
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-05-27 22:17:26 +1200

    Moved all the vehicle routes into the listings route

--- src/app/routes/listing.routes.ts
+++ src/app/routes/listing.routes.ts
@@ -3,11 +3,60 @@ import { rootUrl } from './base.routes';
 import { getListingFilters } from '../controllers/listingController';
 import ajvSchema from '../resources/ajvSchema.json';
 import validateRequestBody from '../middlewares/validateRequestBody';
+import {
+	getAllVehicleListings,
+	postVehicle,
+	getVehicleListing,
+	updateVehicleLising,
+	deleteVehicleListing,
+} from '../controllers/vehicleController';
+import validateRequest from '../middlewares/requestAuthentication/validateRequest';
 
 const listingRoutes = (app: Express) => {
-	app.route(rootUrl + '/listing/filters').get((req, res, next) => {
+	app.route(rootUrl + '/listings/filters').get((req, res, next) => {
 		validateRequestBody(req, res, next, ajvSchema.emptyBody);
 	}, getListingFilters);
+
+	app.route(rootUrl + '/listings')
+		.get((req, res, next) => {
+			validateRequestBody(req, res, next, ajvSchema.emptyBody);
+		}, getAllVehicleListings)
+		.post(
+			validateRequest,
+			(req, res, next) => {
+				validateRequestBody(
+					req,
+					res,
+					next,
+					ajvSchema.postVehicleListing
+				);
+			},
+			postVehicle
+		);
+
+	app.route(rootUrl + '/listings/:id')
+		.get((req, res, next) => {
+			validateRequestBody(req, res, next, ajvSchema.emptyBody);
+		}, getVehicleListing)
+		.patch(
+			validateRequest,
+			(req, res, next) => {
+				validateRequestBody(
+					req,
+					res,
+					next,
+					ajvSchema.updateVehicleListing
+				);
+			},
+			updateVehicleLising
+		)
+		.delete(
+			validateRequest,
+			(req, res, next) => {
+				validateRequestBody(req, res, next, ajvSchema.userIdOnly);
+			},
+			deleteVehicleListing
+		);
 };
 
 export default listingRoutes;
