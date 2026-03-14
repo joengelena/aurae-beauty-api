00aae3d46d8348edcbf28552a040c85b031eb30e
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2026-03-14 13:45:05 +1300

    Revert change

--- src/app/repositories/listingRepository/mapListingAttributesDbToObject.ts
+++ src/app/repositories/listingRepository/mapListingAttributesDbToObject.ts
@@ -4,12 +4,6 @@ function mapListingAttributesDbToObject(
 	listingAttributes: any[],
 ): ListingAttribute[] {
 	return listingAttributes.map((listingAttribute) => {
-		if (listingAttribute.name === 'cylinders') {
-			return {
-				name: listingAttribute.name,
-				attributeValues: listingAttribute.attribute_values as number[],
-			};
-		}
 		return {
 			name: listingAttribute.name,
 			attributeValues: listingAttribute.attribute_values as string[],
